dc0167f02a108a3bea753d9fcf04ae82a9efd30b
Asewlal01 <damiansewlal@gmail.com>
2025-02-19 11:02:33 +0100

    Trade Convolutional model now inherits from base convolutional model

---
+++ models/ConvolutionalModels/TradeConvolve.py
@@ -0,0 +1,27 @@
+\
+from models.ConvolutionalModels.BaseConvolve import BaseConvolve
+
+class TradeConvolve(BaseConvolve):
+    """
+    Convolutional Neural Network for predicting the Closing Price of a stock using the Trade data. This model assumes
+    that the trade data has prices and volumes, which are given in their own channels.
+    """
+    def __init__(self, input_size: int, conv_channels: list[int], fc_neurons: list[int],
+                 kernel_size: int, stride: int=1, padding: int=0, dilation: int=1):
+        """
+        Initializes the Convolutional Neural Network for predicting the Closing Price of a stock using the Trade
+        data.
+
+        Parameters
+        ----------
+        input_size : Number of sequence steps in the input data
+        conv_channels : Number of channels after each convolutional layer
+        fc_neurons : Number of neurons in each fully connected layer
+        kernel_size : Size of the kernel for the convolutional layers. Assumed to be constant for all layers.
+        stride : Stride for the convolutional layers. Assumed to be constant for all layers
+        padding : Padding for the convolutional layers. Assumed to be constant for all layers
+        dilation : Dilation for the convolutional layers. Assumed to be constant for all layers
+        """
+
+        super(TradeConvolve, self).__init__(1, input_size, conv_channels, fc_neurons,
+                                            kernel_size, stride, padding, dilation)
