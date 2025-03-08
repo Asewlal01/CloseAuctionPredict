a8d28315f5ce1c1d1ae233bb139c783fbbef8ab0
Asewlal01 <damiansewlal@gmail.com>
2025-03-08 17:59:05 +0100

    Implemented logistic regression model that uses trading data

---
+++ src/Models/LogisticRegression/TradeLogisticRegression.py
@@ -0,0 +1,17 @@
+from Models.LogisticRegression.BaseLogisticRegression import BaseLogisticRegression
+
+class TradeLogisticRegression(BaseLogisticRegression):
+    """
+    Logistic Regression model for predicting the direction of the stock price.
+    """
+    def __init__(self, sequence_size: int):
+        """
+        Initializes the Logistic Regression Model.
+
+        Parameters
+        ----------
+        sequence_size : Number of time steps in the input data
+        """
+        # Trade data has 5 features: Open, High, Low, Close, Volume
+        feature_size = 5
+        super(TradeLogisticRegression, self).__init__(feature_size, sequence_size)
