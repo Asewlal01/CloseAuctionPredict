42ce6f0d394633b849b9998d5a15163c30f1a2eb
Asewlal01 <damiansewlal@gmail.com>
2025-03-10 14:40:44 +0100

    Added docstrings to methods of ExpectedProfit class

--- src/Metrics/ExpectedProfit.py
+++ src/Metrics/ExpectedProfit.py
@@ -2,7 +2,21 @@ import torch.nn as nn
 import torch
 
 class ExpectedProfit(nn.Module):
-    def __init__(self, k=50, init_tau=0.5, requires_tau_grad=True, lr=1e-3):
+    """
+    This metric computes the expected profit of a trading strategy. The optimal threshold can be optimized using during
+    training.
+    """
+    def __init__(self, k: int=50, init_tau: float=0.5, requires_tau_grad: bool=True, lr: float=1e-3):
+        """
+        Initialize the ExpectedProfit object.
+
+        Parameters
+        ----------
+        k : Value that handles the smoothness of the sigmoid function
+        init_tau : Initial value of the threshold
+        requires_tau_grad : Boolean indicating whether the threshold should be optimized
+        lr : learning rate for the optimization of the threshold
+        """
         super(ExpectedProfit, self).__init__()
         self.k = k
         self.requires_tau_grad = requires_tau_grad
