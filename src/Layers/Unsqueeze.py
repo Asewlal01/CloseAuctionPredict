fe8611a50b24b35e196e772be2c92c6b42933705
Asewlal01 <damiansewlal@gmail.com>
2025-03-14 16:22:38 +0100

    Moved Unsqueeze layer to its own file

---
+++ src/Layers/Unsqueeze.py
@@ -0,0 +1,27 @@
+from torch import nn
+
+class Unsqueeze(nn.Module):
+    """
+    Layer designed to unsqueeze a tensor. This is needed when the input is not given as a batch, which may cause
+    errors in the forward pass. Dimensions go from (N,) to (1, N) for example.
+    """
+
+    def __init__(self, expected_dim: int):
+        """
+        Initializes the unsqueeze layer.
+
+        Parameters
+        ----------
+        expected_dim : Expection dimension of the input tensor. This layer will add a dimension if the input tensor
+        has a dimension of `expected_dim - 1`.
+
+        """
+        super(Unsqueeze, self).__init__()
+        self.expected_dim = expected_dim
+
+    def forward(self, x):
+        # If only one sample is given, add a batch dimension
+        if x.dim() == self.expected_dim - 1:
+            return x.unsqueeze(0)
+        return x
+
--- src/Models/BaseModel.py
+++ src/Models/BaseModel.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+from Layers.Unsqueeze import Unsqueeze
 
 class BaseModel(nn.Module):
     """
@@ -7,19 +8,17 @@ class BaseModel(nn.Module):
     training functionality and other useful methods that are common to all the Models.
     """
 
-    def __init__(self, expected_dim: int, dropout: float = 0.5):
+    def __init__(self, expected_dim: int):
         """
         Initializes the BaseModel class. It should be called by all the classes that inherit from this class.
 
         Parameters
         ----------
         expected_dim : Expected number of dimensions after unsqueezing the input tensor.
-        dropout : Dropout rate to use in the fully connected layers.
         """
         super(BaseModel, self).__init__()
-        self.layers = [Unsqueeze(expected_dim)]
-        self.output_dim = None
-        self.dropout = dropout
+        self.layers: list[nn.Module] = [Unsqueeze(expected_dim)]
+        self.output_dim: int = 0
         self.build_model()
         self.output_layer()
 
@@ -54,31 +53,11 @@ class BaseModel(nn.Module):
         Adds the output layer to the model. This method requires self.output_dim to be defined, as it is the dimension
         of the layer before the output layer.
         """
-        if self.output_dim is None:
+        if self.output_dim is 0:
             raise AttributeError("The attribute `output_dim` is not defined in the model.")
-
-        self.layers.append(
-            nn.Dropout(self.dropout)
-        )
 
         self.layers.append(
             nn.Linear(self.output_dim, 1)
         )
+
         self.layers = nn.ModuleList(self.layers)
-
-
-class Unsqueeze(nn.Module):
-    """
-    Layer designed to unsqueeze a tensor. This is needed when the input is not given as a batch, which may cause
-    errors in the forward pass.
-    """
-
-    def __init__(self, expected_dim: int):
-        super(Unsqueeze, self).__init__()
-        self.expected_dim = expected_dim
-
-    def forward(self, x):
-        # If only one sample is given, add a batch dimension
-        if x.dim() == self.expected_dim - 1:
-            return x.unsqueeze(0)
-        return x
