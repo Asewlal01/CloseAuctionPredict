5e544360d26e75be5509206d4598ecb5abfd9438
Asewlal01 <damiansewlal@gmail.com>
2025-03-31 20:52:16 +0200

    Logistic regression has been changes to linear regression

--- src/Models/LinearRegression/TradeLinearRegression.py
+++ src/Models/LinearRegression/TradeLinearRegression.py
@@ -13,5 +13,5 @@ class TradeLogisticRegression(BaseLogisticRegression):
         sequence_size : Number of time steps in the input data
         """
         # Trade data has 5 features: Open, High, Low, Close, Volume
-        feature_size = 5
+        feature_size = 2
         super(TradeLogisticRegression, self).__init__(feature_size, sequence_size)
