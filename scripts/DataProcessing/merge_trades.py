b3cec167c148f7afbde1eae5fc2e33ee465008cf
Asewlal01 <damiansewlal@gmail.com>
2025-04-03 08:42:55 +0200

    Trademerger has been updated to merge based on months

--- scripts/DataProcessing/merge_trades.py
+++ scripts/DataProcessing/merge_trades.py
@@ -1,14 +1,8 @@
 from DataProcessing.TradeMerger import TradeMerger
 
-sequence_size = 7 * 60
-trades_dir = 'data/processed/aggregated_trades'
-save_dir = 'data/processed/merged_trades'
-trade_dates = 'data/processed/dates.csv'
-train_months = 0.75
-val_months = 0.15
-normalization_scheme = 'simple_returns'
+trades_dir = 'data/aggregated_trades'
+save_dir = 'data/merged_trades'
 
-merger = TradeMerger(sequence_size, normalization_scheme, trades_dir, save_dir, trade_dates, train_months, val_months)
-merger.add_stocks()
-merger.split_data()
-merger.save_data()
+trade_merger = TradeMerger(trades_dir, save_dir)
+trade_merger.process_all()
+
