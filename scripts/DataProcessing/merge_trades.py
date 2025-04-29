eda987f909f5817b4a4e77b9071c8c9075cefa27
Asewlal01 <damiansewlal@gmail.com>
2025-04-29 12:06:59 +0200

    Merger has been renamed to assembler, and now also uses LOB data

--- scripts/DataProcessing/merge_trades.py
+++
@@ -1,8 +0,0 @@
-from DataProcessing.TradeMerger import TradeMerger
-
-trades_dir = 'data/aggregated_trades'
-save_dir = 'data/merged_trades'
-
-trade_merger = TradeMerger(trades_dir, save_dir)
-trade_merger.process_all()
-
