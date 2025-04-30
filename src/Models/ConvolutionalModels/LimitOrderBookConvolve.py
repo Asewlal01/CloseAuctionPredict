c67738e5e49b38ff38618eac3921d8ca3a4a87ff
Asewlal01 <damiansewlal@gmail.com>
2025-04-30 13:43:48 +0200

    Implemented cnn which uses limit order book to predict

---
+++ src/Models/ConvolutionalModels/LimitOrderBookConvolve.py
@@ -0,0 +1,28 @@
+from Models.ConvolutionalModels.BaseConvolve import BaseConvolve
+
+class LimitOrderBookConvolve(BaseConvolve):
+    """
+    Convolutional Neural Network for predicting the Closing Price of a stock using the Limit Order Book data, and
+    Trade data.
+    """
+    def __init__(self, sequence_size: int, conv_channels: list[int], fc_neurons: list[int],
+                 kernel_size: list[int], stride: int=1, padding: int=0, dilation: int=1, dropout: float=0):
+        """
+        Initializes the Convolutional Neural Network for predicting the Closing Price of a stock using the Trade
+        data.
+
+        Parameters
+        ----------
+        sequence_size : Number of sequence steps in the input data
+        conv_channels : Number of channels after each convolutional layer
+        fc_neurons : Number of neurons in each fully connected layer
+        kernel_size : Size of the kernel for each convolutional layer.
+        stride : Stride for the convolutional layers. Assumed to be constant for all layers
+        padding : Padding for the convolutional layers. Assumed to be constant for all layers
+        dilation : Dilation for the convolutional layers. Assumed to be constant for all layers
+        dropout : Dropout rate to use in the fully connected
+        """
+
+        feature_size = 26
+        super().__init__(feature_size, sequence_size, conv_channels, fc_neurons,
+                                            kernel_size, stride, padding, dilation, dropout)
