de541f686596d2fa0e2fa9210146a468c1a50859
Asewlal01 <damiansewlal@gmail.com>
2025-05-01 18:19:10 +0200

    Added LinearRegression, LSTM and Transformer models with LOB data

---
+++ src/Models/ReccurentModels/LimitOrderBookLSTM.py
@@ -0,0 +1,23 @@
+from Models.ReccurentModels.BaseLSTM import BaseLSTM
+
+class LimitOrderBookLSTM(BaseLSTM):
+    """
+    LSTM Neural Network for predicting the Closing Price of a stock using the Trade data. Input data
+    is assumed to be a 2D tensor with the columns representing the return and volume of the stock respectively. The
+    rows are assumed to be the time steps. The output of the model is a 1D tensor representing the predicted closing
+    price of the stock.
+    """
+
+    def __init__(self,  hidden_size: int, lstm_size: int, fc_neurons: list[int], dropout: float = 0.5):
+        """
+        Initializes the LSTM Neural Network.
+
+        Parameters
+        ----------
+        hidden_size : Dimension of the hidden state of the LSTM
+        lstm_size : Number of LSTM layers
+        fc_neurons : Number of nodes in the fully connected layers. Can be a list of integers or a single integer.
+        """
+        # Trade data has 5 features: close, high, low, open, volume, vwap
+        feature_size = 26
+        super().__init__(feature_size,  hidden_size, lstm_size, fc_neurons, dropout)
