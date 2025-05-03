62497131decd9ea086c9e9b1b9fe58357989a66e
Asewlal01 <damiansewlal@gmail.com>
2025-05-03 11:09:29 +0200

    Renamed View to Reshape

--- src/Layers/ReshapeLayer.py
+++ src/Layers/ReshapeLayer.py
@@ -2,14 +2,14 @@ from torch import nn
 import torch
 
 
-class ViewLayer(nn.Module):
+class ReshapeLayer(nn.Module):
     """
     Layer designed to reshape a tensor. This is needed because input is typically given as (batch, sequence, features).
     However, Linear layers require the input to be (batch, features).
     """
     def __init__(self):
-        super(ViewLayer, self).__init__()
+        super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size = x.shape[0]
-        return x.view(batch_size, -1)
+        return x.reshape(batch_size, -1)
