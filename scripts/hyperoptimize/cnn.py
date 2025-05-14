1c47974d7ac2d941aa3d197220dc5abd8e681857
Asewlal01 <damiansewlal@gmail.com>
2025-05-14 22:01:03 +0200

    cnn script now runs in parallel to maximise gpu usage

--- scripts/hyperoptimize/cnn.py
+++ scripts/hyperoptimize/cnn.py
@@ -2,6 +2,7 @@ from Models.ConvolutionalModels.LobConvolve import LobConvolve
 from Modeling.HyperOptimizer import HyperOptimizer
 from Modeling.DatasetManagers.BaseDatasetManager import BaseDatasetManager
 import os
+import multiprocessing
 
 def model_parameters(trial, sequence_size):
     # Number of layers
@@ -12,8 +13,8 @@ def model_parameters(trial, sequence_size):
     conv_channels = []
     kernel_sizes = []
     for i in range(n_conv_layers):
-        # Every convolutional channel is a multiple of 16
-        conv_channels_multiple = trial.suggest_int(f'conv_channels_multiple_{i}', 1, 8)
+        # Every convolutional channel is a multiple of 8
+        conv_channels_multiple = trial.suggest_int(f'conv_channels_multiple_{i}', 4, 64)
         conv_channels.append(conv_channels_multiple * 8)
 
         # Kernel size is an uneven number with a minimum of 3 (2n + 1)
@@ -23,8 +24,8 @@ def model_parameters(trial, sequence_size):
     # Fully connected layer parameters
     fc_neurons = []
     for i in range(n_fc_layers):
-        # Every neuron in the layer is a multiple of 16
-        fc_neurons_multiple = trial.suggest_int(f'fc_neurons_multiple_{i}', 1, 8)
+        # Every neuron in the layer is a multiple of 8
+        fc_neurons_multiple = trial.suggest_int(f'fc_neurons_multiple_{i}', 1, 64)
         fc_neurons.append(fc_neurons_multiple * 8)
 
     # Dropout rate are multiple of 0.1
@@ -43,9 +44,9 @@ def model_parameters(trial, sequence_size):
     return model, sequence_size
 
 def objective(trial):
-    # Sequence size is a multiple of 5
-    sequence_size_multiple = trial.suggest_int('sequence_size_multiple', 12, 72)
-    sequence_size = sequence_size_multiple * 5
+    # Sequence size is a multiple of 8
+    sequence_size_multiple = trial.suggest_int('sequence_size_multiple', 7, 45)
+    sequence_size = sequence_size_multiple * 8
 
     # Model parameters
     model, sequence_size = model_parameters(trial, sequence_size)
@@ -55,27 +56,37 @@ def objective(trial):
     lr = 10 ** lr_power
 
     # Number of epochs is multiple of 5
-    epochs_multiple = trial.suggest_int('epochs_multiple', 1, 10)
+    epochs_multiple = trial.suggest_int('epochs_multiple', 1, 6)
     epochs = epochs_multiple * 5
 
-    # Batch size is a power of 2
-    batch_size_power = trial.suggest_int('batch_size_power', 4, 8)
-    batch_size = 2 ** batch_size_power
+    return model, epochs, lr, sequence_size
 
-    return model, epochs, lr, batch_size, sequence_size
-
-if __name__ == '__main__':
+def run_study():
     path_to_data = 'data/intraday'
 
     # Save path of results
     results = 'results/hyperparameters_intraday'
     name = 'cnn'
+
+    # NUmber of evaluations
+    trials = 100
+
+    # Save path of results
     os.makedirs(results, exist_ok=True)
 
     # Train the model
     train_manager = BaseDatasetManager(path_to_data, 1)
     test_manager = BaseDatasetManager(path_to_data, 1)
     optimizer = HyperOptimizer(train_manager, test_manager, '2021-1', 11)
-    optimizer.optimize(objective, name, results, n_trials=100)
+    optimizer.optimize(objective, name, results, n_trials=trials)
+
+if __name__ == '__main__':
+    processes = 4
+    processes = [multiprocessing.Process(target=run_study) for _ in range(processes)]
+
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
 
 
