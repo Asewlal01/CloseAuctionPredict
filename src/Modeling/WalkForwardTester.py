c6f833622b65b8ef523fcb9d6add21f1233009e2
Asewlal01 <damiansewlal@gmail.com>
2025-05-14 12:40:17 +0200

    WalkForwardTester has been updated to match new functionality of DatasetManagers

--- src/Modeling/WalkForwardTester.py
+++ src/Modeling/WalkForwardTester.py
@@ -1,28 +1,31 @@
 import torch
 from torch.nn import BCEWithLogitsLoss
+from torch.utils.data import DataLoader
+
 from Models.BaseModel import BaseModel
-from Modeling.DatasetManager import DatasetManager, DatasetTuple
+from Modeling.DatasetManagers.BaseDatasetManager import BaseDatasetManager, convert_to_classification
 from Metrics.ProfitCalculator import ProfitCalculator
 
 
 class WalkForwardTester:
-    """
-
-    """
-    def  __init__(self, model: BaseModel, dataset_manager: DatasetManager):
+    def  __init__(self, model: BaseModel, train_manager: BaseDatasetManager, test_manager: BaseDatasetManager,
+                  sequence_size: int = 360):
         """
         Initializes the WalkForwardTester class.
 
         Parameters
         ----------
         model : Model to be tested
-        dataset_manager : The DatasetManager instance that handles the dataset.
+        train_manager : DatasetManager to be used for training
+        test_manager : DatasetManager to be used for testing
         """
         self.model = model
-        self.dataset_manager = dataset_manager
+        self.train_manager = train_manager
+        self.test_manager = test_manager
+        self.sequence_size = sequence_size
         self.metric = ProfitCalculator()
 
-    def train(self, epochs, learning_rate, verbose=False) -> None:
+    def train(self, epochs, learning_rate, batch_size, verbose=False) -> None:
         """
         Trains the model using walk forward testing. It trains the model on the training data and validates it on the
         validation data.
@@ -31,13 +34,15 @@ class WalkForwardTester:
         ----------
         epochs : Number of epochs to train the model.
         learning_rate : Learning rate to be used for training.
+        batch_size : Size of the batch to be used for training the model.
         verbose : Whether to print training information.
         """
         # Get the training and validation data
-        train_data = self.dataset_manager.get_training_data(binary_classification=True)
+        train_data = self.train_manager.get_dataset(sequence_size=self.sequence_size,
+                                                    normalize=True, binary_classification=False)
 
         # Train the model
-        train(self.model, train_data, epochs, learning_rate, verbose)
+        train(self.model, train_data, epochs, learning_rate, batch_size, verbose)
 
     def evaluate_on_train(self) -> tuple[float, float, float]:
         """
@@ -49,7 +54,8 @@ class WalkForwardTester:
         Profit, accuracy and loss of the model on the training data.
         """
         # Get the training and validation data
-        train_data = self.dataset_manager.get_training_data(binary_classification=False)
+        train_data = self.train_manager.get_dataset(sequence_size=self.sequence_size,
+                                                    normalize=True, binary_classification=False)
 
         evaluation = evaluate(self.model, train_data)
 
@@ -64,15 +70,16 @@ class WalkForwardTester:
         Profit, accuracy and loss of the model on the test data.
         """
         # Get the training, validation and test data
-        test_data = self.dataset_manager.get_test_data()
+        test_data = self.test_manager.get_dataset(sequence_size=self.sequence_size,
+                                                  normalize=True, binary_classification=False)
 
         evaluation = evaluate(self.model, test_data)
 
         return evaluation
 
 
-def train(model: BaseModel, train_data: list[DatasetTuple], epochs: int, learning_rate: float,
-          verbose=True) -> None:
+def train(model: BaseModel, train_data: tuple[torch.Tensor, torch.Tensor], epochs: int, learning_rate: float,
+          batch_size: int = 32, verbose=True) -> None:
     """
     Train the model using the given training data. It uses the BCEWithLogitsLoss function to compute the loss and
     Adam optimizer to optimize the model. The model is trained for the given number of epochs. This training scheme
@@ -85,39 +92,48 @@ def train(model: BaseModel, train_data: list[DatasetTuple], epochs: int, learnin
     train_data : The training data to be used for training the model.
     epochs : Number of epochs to train the model.
     learning_rate : Learning rate to be used for training the model.
+    batch_size : Size of the batch to be used for training the model.
     verbose : Whether to print training information.
     """
 
     # Initialize the optimizer
-    loss = BCEWithLogitsLoss()
+    loss = BCEWithLogitsLoss(reduction='none')
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     model.train()
-    average_loss = 0
-    total_samples = 0
-    for i, (x, y, z) in enumerate(train_data):
-        x = x.to(model.device)
-        y = y.to(model.device)
-        z = z.to(model.device)
+
+    loader = create_loader(train_data, batch_size)
+
+    for epoch in range(epochs):
         total_loss = 0
-        for epoch in range(epochs):
+        samples = 0
+        for x, y, weights in loader:
+            x = x.to(model.device)
+            y = y.to(model.device)
+            weights = weights.to(model.device)
+
+            # Training requires y to be binary
+            y = convert_to_classification(y)
+
             # Forward pass
-            y_pred = model(x, z)
-            loss_value = loss(y_pred, y)
-            total_loss = loss_value.item()
+            y_pred = model(x)
+            loss_per_sample = loss(y_pred, y)
+            weighted_loss = weights * loss_per_sample
+            loss_value = weighted_loss.sum()
+
+
+            total_loss += loss_value.item()
+            samples += len(y)
 
             # Backward pass
             optimizer.zero_grad()
             loss_value.backward()
             optimizer.step()
 
-        average_loss += total_loss * len(y)
-        total_samples += len(y)
-
         if verbose:
-            print(f"Batch {i+1}/{len(train_data)} has average Loss of : {average_loss/total_samples:.4f}")
+            print(f"Epoch {epoch+1}/{epochs} has average Loss of : {total_loss:.4f}")
 
-def evaluate(model, dataset) -> tuple[float, float, float]:
+def evaluate(model, dataset, batch_size=1024) -> tuple[float, float, float]:
     """
     Evaluate the model using the given dataset and metric. It also computes the accuracy of the model by comparing
     the sign of the predicted and actual values.
@@ -126,6 +142,7 @@ def evaluate(model, dataset) -> tuple[float, float, float]:
     ----------
     model : The model to be evaluated.
     dataset : The dataset to be evaluated.
+    batch_size : The batch size to be used for evaluation.`
 
     Returns
     -------
@@ -133,7 +150,8 @@ def evaluate(model, dataset) -> tuple[float, float, float]:
     """
     # Setup
     profit_calculator = ProfitCalculator()
-    loss_fn = BCEWithLogitsLoss()
+    loss_fn = BCEWithLogitsLoss(reduction='none')
+    loader = create_loader(dataset, batch_size=batch_size)
     model.eval()
 
     with torch.no_grad():
@@ -141,29 +159,67 @@ def evaluate(model, dataset) -> tuple[float, float, float]:
         average_accuracy = 0
         average_loss = 0
         total_samples = 0
-        for X, y, z in dataset:
-            X = X.to(model.device)
+        for x, y, weights in loader:
+            x = x.to(model.device)
             y = y.to(model.device)
-            z = z.to(model.device)
-            y_pred = model(X, z)
+            weights = weights.to(model.device)
+            y_pred = model(x)
 
             profit = profit_calculator(y_pred, y)
 
             # Accuracy checks if sign match
-            signs = ((y_pred * y) > 0).float()
-            accuracy = ((y_pred * y) > 0).float().mean()
+            accuracy = ((y_pred * y) > 0).float().sum()
 
             # Loss requires y to be binary
-            y = torch.where(y > 0, 1, 0).float()
+            y = convert_to_classification(y)
             loss = loss_fn(y_pred, y)
+            loss = loss * weights
+            loss = loss.sum()
 
             average_profit += profit.item() * len(y)
-            average_accuracy += accuracy.item() * len(y)
-            average_loss += loss.item() * len(y)
+            average_accuracy += accuracy.item()
+            average_loss += loss.item()
             total_samples += len(y)
 
         average_profit /= total_samples
         average_accuracy /= total_samples
-        average_loss /= total_samples
 
         return average_profit, average_accuracy, average_loss
+
+def create_loader(dataset: tuple[torch.Tensor, torch.Tensor], batch_size: int = 32) -> DataLoader:
+    """
+    Create a DataLoader for the given dataset.
+
+    Parameters
+    ----------
+    dataset : The dataset to be used for creating the DataLoader.
+    batch_size : The batch size to be used for creating the DataLoader.
+
+    Returns
+    -------
+    DataLoader for the given dataset.
+    """
+    x, y = dataset
+    weights = compute_sample_weights(y)
+
+    # Convert the dataset to a PyTorch Dataset
+    dataset = torch.utils.data.TensorDataset(x, y, weights)
+
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+def compute_sample_weights(y):
+    """
+    Compute the sample weights for the given dataset. The sample weights are used to balance the dataset. The samples
+    are weighted based on the number of samples in each class. The weights are computed as the inverse of the
+    """
+    # Compute log return from percentage change
+    log_returns = torch.log(y + 1)
+
+    # Convert to absolute values
+    abs_return = torch.abs(log_returns)
+    total_return = abs_return.sum(dim=0, keepdim=True)
+
+    # Compute the sample weights
+    sample_weights = abs_return / total_return
+
+    return sample_weights
