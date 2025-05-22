ecbcd851643e353dfee4de222496ac52af561634
Asewlal01 <damiansewlal@gmail.com>
2025-05-22 10:56:25 +0200

    Fixed learning rates of models

--- scripts/modeling/TradeLOB/training/transformer.py
+++ scripts/modeling/TradeLOB/training/transformer.py
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     sequence_size = 120
     model = get_model(sequence_size)
     epochs = 100
-    lr = 1e-3
+    lr = 1e-4
     name = 'transformer'
     run_training(model, epochs, lr, sequence_size, name)
