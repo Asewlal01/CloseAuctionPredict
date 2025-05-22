039f4e0135ee005ac9d89fef3ed22022b99db430
Asewlal01 <damiansewlal@gmail.com>
2025-05-22 15:43:24 +0200

    Fixed parameters in transformer

--- scripts/modeling/intraday/training/transformer.py
+++ scripts/modeling/intraday/training/transformer.py
@@ -6,10 +6,10 @@ def get_model(sequence_size):
         sequence_size=sequence_size,
         embedding_size=32,
         num_heads=4,
-        dim_feedforward=64,
-        num_layers=4,
+        dim_feedforward=128,
+        num_layers=2,
         fc_neurons = [128, 64],
-        dropout = 0.1,
+        dropout = 0.2,
     )
     model.to('cuda')
     return model
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     sequence_size = 240
     model = get_model(sequence_size)
     epochs = 100
-    lr = 1e-3
+    lr = 1e-4
     name = 'transformer'
     run_training(model, epochs, lr, sequence_size, name)
