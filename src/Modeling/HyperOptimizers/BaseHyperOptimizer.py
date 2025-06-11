ee9b48b3d2accffa0de8fe468e77f55dc8415528
Asewlal01 <damiansewlal@gmail.com>
2025-06-11 19:07:56 +0200

    Moved specific values to config

--- src/Modeling/HyperOptimizers/BaseHyperOptimizer.py
+++ src/Modeling/HyperOptimizers/BaseHyperOptimizer.py
@@ -4,22 +4,8 @@ from Models.BaseModel import BaseModel
 import optuna, os, torch, gc
 import yaml
 
-# Constants for sequence length
-MIN_SEQUENCE_LENGTH = 30
-MAX_SEQUENCE_LENGTH = 420
-LENGTH_STEP_SIZE = 10
-
-# Constants for dataset sizes
-TRAIN_SIZE = 189
-VALIDATION_SIZE = 20
-
-# Training parameters
-EPOCHS = 100
-LEARNING_RATE = 1e-3
-
 # Path to the config file
 CONFIG_PATH = 'configs/tuning_config.yaml'
-
 
 class BaseHyperOptimizer:
     """
@@ -40,8 +26,10 @@ class BaseHyperOptimizer:
         self.path_to_datasets = path_to_datasets
         self.n_sets = n_sets
         self.prefix = prefix
+        self.config = None
+        self.name = None
 
-    def optimize(self, name: str, save_path: str, n_trials: int = 100):
+    def optimize(self, name: str=None, save_path: str=None, n_trials: int=None):
         """
         Optimize the hyperparameters of the model.
 
@@ -51,9 +39,13 @@ class BaseHyperOptimizer:
         save_path : The path to save the results.
         n_trials : The number of trials to run.  Default is 100.
         """
+
+        # Set default values for save_path, and n_trials if not provided
+        save_path = save_path if save_path is not None else self.config['run']['save_path']
+        n_trials = n_trials if n_trials is not None else self.config['run']['n_trials']
+
         # Make sure the save path exists
         os.makedirs(save_path, exist_ok=True)
-
 
         storage = f'sqlite:///{save_path}/results.db'
         study = optuna.create_study(direction='minimize',
@@ -92,8 +84,9 @@ class BaseHyperOptimizer:
             Average loss over the walk-forward validation.
             """
             # Sequence size is needed to construct the model
-            sequence_size = trial.suggest_int('sequence_size', MIN_SEQUENCE_LENGTH, MAX_SEQUENCE_LENGTH,
-                                              step=LENGTH_STEP_SIZE)
+            min_sequence, max_sequence, step_sequence = self.config['common']['sequence_size']
+            sequence_size = trial.suggest_int('sequence_size', min_sequence, max_sequence,
+                                              step=step_sequence)
 
             # Create the model using the model function
             model = self.generate_model(trial, sequence_size)
@@ -101,19 +94,30 @@ class BaseHyperOptimizer:
             # Initialize metric to optimize
             test_loss = 0
 
+            # Generate all dataset managers
+            dataset_managers = []
             for i in range(self.n_sets):
-                path_to_set = os.path.join(self.path_to_datasets, f'{self.prefix}{i + 1}')
-
                 # Initialize dataset managers
+                path_to_set = os.path.join(self.path_to_datasets, f'{self.prefix}{i + 1}')
                 dataset_manager = ClosingDatasetManager(path_to_set)
-                dataset_manager.setup_dataset(TRAIN_SIZE, VALIDATION_SIZE)
+                training_params = self.config['training_params']
 
+                train_size = training_params['train_size']
+                validation_size = training_params['validation_size']
+                dataset_manager.setup_dataset(train_size, validation_size)
+                dataset_managers.append(dataset_manager)
+
+            for i in range(self.n_sets):
                 # Initialize model runner
+                dataset_manager = dataset_managers[i]
                 model_runner = ModelRunner(model, dataset_manager, sequence_size,
                                            use_trading=True, use_exogenous=True)
 
                 # Training the model
-                model_runner.train(EPOCHS, LEARNING_RATE)
+                training_params = self.config['training_params']
+                epochs = training_params['epochs']
+                learning_rate = training_params['learning_rate']
+                model_runner.train(epochs, learning_rate)
 
                 # Evaluate the model on the test set
                 loss = model_runner.evaluate_on_test()
@@ -151,15 +155,42 @@ class BaseHyperOptimizer:
         """
 
         with open(CONFIG_PATH, 'r') as f:
-            config = yaml.safe_load(f)['run']
+            config = yaml.safe_load(f)
 
-        path_to_datasets = config['path_to_dataset']
-        n_sets = config['n_sets']
-        prefix = config['prefix']
-        trials = config['trials']
-        save_path = config['save_path']
+        # Get dataset configuration
+        dataset_config = config['dataset']
+        path_to_datasets = dataset_config['path_to_dataset']
+        n_sets = dataset_config['n_sets']
+        prefix = dataset_config['prefix']
 
-        return cls(path_to_datasets=path_to_datasets, n_sets=n_sets, prefix=prefix), save_path, trials
+        # Return object
+        optimizer = cls(path_to_datasets, n_sets, prefix)
+        optimizer.config = config
+
+        return optimizer
+
+    def generate_common_parameter(self, trial: optuna.Trial) -> tuple:
+        """
+        Generate parameters that are common to all models.
+
+        Returns
+        -------
+        A tuple containing the number of neurons in each fully connected layer.
+        """
+        common_config = self.config['common']
+        layers_min, layers_max, layers_step = common_config['fc_layers']
+        neurons_min, neurons_max, neurons_step = common_config['fc_neurons']
+
+        fc_neurons = []
+        for layer in range(layers_min, layers_max + 1, layers_step):
+            neurons = trial.suggest_int(f'fc_neurons_{layer}', neurons_min, neurons_max, step=neurons_step)
+            fc_neurons.append(neurons)
+
+        # Dropout
+        dropout_min, dropout_max, dropout_step = common_config['dropout']
+        dropout = trial.suggest_float('dropout', dropout_min, dropout_max, step=dropout_step)
+
+        return fc_neurons, dropout
 
 
 def clear_memory():
