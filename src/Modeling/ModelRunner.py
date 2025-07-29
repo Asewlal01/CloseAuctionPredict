c04cc2694c5eac9ff91e9d198746ccb089c32646
Asewlal01 <damiansewlal@gmail.com>
2025-07-29 08:58:05 +0200

    Now allows to select a prediction horizon

--- src/Modeling/ModelRunner.py
+++ src/Modeling/ModelRunner.py
@@ -2,7 +2,6 @@ import torch
 from Models.BaseModel import BaseModel
 from Modeling.DatasetManagers.BaseDatasetManager import BaseDatasetManager, BaseSampleTuple
 from copy import deepcopy
-import gc
 
 DatasetTuple = tuple[list[BaseSampleTuple], list[BaseSampleTuple], list[BaseSampleTuple]]
 
@@ -16,7 +15,8 @@ class ModelRunner:
     on the train, test and validation datasets.
     """
     def __init__(self, model: BaseModel, dataset_manager: BaseDatasetManager,
-                 sequence_size: int=420, use_trading: bool=False, use_exogenous: bool=False):
+                 sequence_size: int=420, use_trading: bool=False, use_exogenous: bool=False,
+                 horizon: int=0):
         """
         Initialize the ModelRunner.
 
@@ -27,15 +27,17 @@ class ModelRunner:
         sequence_size : The size of the sequences to be used for training and evaluation. Default is 420.
         use_trading : Whether to use trading features in the model. Default is True.
         use_exogenous : Whether to use exogenous features in the model. Default is False.
+        horizon : The horizon for the prediction. Default is 0, which means no horizon.
         """
 
         self.model = model
         self.dataset_manager = dataset_manager
 
         self.train_dataset, self.validation_dataset, self.test_dataset = self.get_datasets(sequence_size,
-                                                                                           use_trading, use_exogenous)
+                                                                                           use_trading, use_exogenous,
+                                                                                           horizon)
 
-    def get_datasets(self, sequence_size: int, use_trading: bool, use_exogenous: bool) -> DatasetTuple:
+    def get_datasets(self, sequence_size: int, use_trading: bool, use_exogenous: bool, horizon: int) -> DatasetTuple:
         """
         Get the datasets for training, validation, and testing.
 
@@ -44,6 +46,7 @@ class ModelRunner:
         sequence_size : The size of the sequences to be used for training and evaluation.
         use_trading : Whether to use trading features in the model.
         use_exogenous : Whether to use exogenous features in the model.
+        horizon : The horizon for the prediction. Default is 0, which means no horizon.
 
         Returns
         -------
@@ -55,13 +58,14 @@ class ModelRunner:
         test_dataset = self.dataset_manager.get_test_dataset()
 
         # Filtering each dataset based on the use of trading and exogenous features
-        train_dataset = filter_features(train_dataset, sequence_size, use_trading, use_exogenous)
-        validation_dataset = filter_features(validation_dataset, sequence_size, use_trading, use_exogenous)
-        test_dataset = filter_features(test_dataset, sequence_size, use_trading, use_exogenous)
+        train_dataset = filter_features(train_dataset, sequence_size, use_trading, use_exogenous, horizon)
+        validation_dataset = filter_features(validation_dataset, sequence_size, use_trading, use_exogenous, horizon)
+        test_dataset = filter_features(test_dataset, sequence_size, use_trading, use_exogenous, horizon)
 
         return train_dataset, validation_dataset, test_dataset
 
-    def train(self, epochs: int, learning_rate: float, verbose: bool=False) -> None:
+    def train(self, epochs: int, learning_rate: float,
+              stopping_epochs: int, verbose: bool=False) -> None:
         """
         Train the model on the training dataset.
 
@@ -69,6 +73,7 @@ class ModelRunner:
         ----------
         epochs : Number of epochs to train the model for.
         learning_rate : Learning rate for the optimizer.
+        stopping_epochs : Number of epochs to wait before stopping the training if no improvement is seen.
         verbose : Whether to print training progress. Default is False.
         """
 
@@ -77,7 +82,7 @@ class ModelRunner:
         validation_dataset = self.validation_dataset
 
         # Train the model
-        train_model(self.model, train_dataset, validation_dataset, epochs, learning_rate, verbose)
+        train_model(self.model, train_dataset, validation_dataset, epochs, learning_rate, stopping_epochs, verbose)
 
     def evaluate_on_train(self) -> float:
         """
@@ -141,7 +146,8 @@ class ModelRunner:
 
 
 def filter_features(dataset: list[BaseSampleTuple],
-                    sequence_size: int, use_trading: bool, use_exogenous: bool) -> list[BaseSampleTuple]:
+                    sequence_size: int, use_trading: bool, use_exogenous: bool,
+                    horizon: int) -> list[BaseSampleTuple]:
     """
     Filter the features in the dataset based on the use of trading and exogenous features.
 
@@ -151,6 +157,7 @@ def filter_features(dataset: list[BaseSampleTuple],
     sequence_size : The size of the sequences to be fed into the model.
     use_trading : Whether to use trading features.
     use_exogenous : Whether to use exogenous features.
+    horizon : The horizon for the prediction. Default is 0, which means no horizon.
 
     Returns
     -------
@@ -163,6 +170,9 @@ def filter_features(dataset: list[BaseSampleTuple],
         # First 22 features are lob, followed by trading features
         if not use_trading:
             x = x[:, :, LOB_FEATURE_INDICES]
+
+        if horizon > 0:
+            x = x[:, :-horizon]  # Remove the last horizon features from the input
 
         # We only use the last sequence_size features for training
         x = x[:, -sequence_size:]
@@ -177,7 +187,8 @@ def filter_features(dataset: list[BaseSampleTuple],
     return filtered_dataset
 
 def train_model(model: BaseModel, train_dataset: list[BaseSampleTuple], validation_dataset: list[BaseSampleTuple],
-                epochs: int, learning_rate: float, verbose=False) -> None:
+                epochs: int, learning_rate: float, stopping_epochs: int,
+                verbose=False) -> None:
     """
     Train the model on the provided dataset.
 
@@ -188,6 +199,7 @@ def train_model(model: BaseModel, train_dataset: list[BaseSampleTuple], validati
     validation_dataset : The dataset to validate the model on.
     epochs : Number of epochs to train the model for.
     learning_rate : Learning rate for the optimizer.
+    stopping_epochs : Number of epochs to wait before stopping the training if no improvement is seen.
     verbose : Whether to print training progress. Default is False.
     """
     # Define the optimizer and loss function
@@ -197,10 +209,10 @@ def train_model(model: BaseModel, train_dataset: list[BaseSampleTuple], validati
     # Best validation loss and state
     best_loss = float('inf')
     best_state = deepcopy(model.state_dict())
+    counter = 0
 
     # Training loop
     for epoch in range(epochs):
-        # Training for one epoch
         train_single_epoch(model, train_dataset, loss_fn, optimizer)
 
         # Get the loss on validation dataset
@@ -208,11 +220,19 @@ def train_model(model: BaseModel, train_dataset: list[BaseSampleTuple], validati
         if validation_loss < best_loss:
             best_loss = validation_loss
             best_state = deepcopy(model.state_dict())
+            counter = 0
+        else:
+            counter += 1
 
         # Print the results is verbose is True
         if verbose:
             train_evaluation = evaluate_model(model, train_dataset, loss_fn)
             print(f'Epoch {epoch + 1}/{epochs}: \n Train Loss: {train_evaluation:.4f}, Validation Loss: {validation_loss:.4f}')
+
+        if counter >= stopping_epochs:
+            if verbose:
+                print(f'Stopping training after {stopping_epochs} epochs without improvement.')
+            break
 
     # Load the best state
     model.load_state_dict(best_state)
@@ -245,7 +265,7 @@ def train_single_epoch(model: BaseModel, train_dataset: list[BaseSampleTuple],
         y = convert_to_classification(y)
 
         # Compute the loss
-        loss = loss_fn(output, y)
+        loss =  loss_fn(output, y)
 
         # Backward pass
         loss.backward()
